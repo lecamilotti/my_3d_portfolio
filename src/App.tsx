54b67530a4dcfd204b4c2534ef02eb338065ae0d
Leandro Camilotti <lecamilotti@gmail.com>
2024-07-10 20:35:47 +0200

    Convert project into a typeScript now need to fix some type issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
 
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from './components';
 
 const App = () => {
   return (
@@ -22,6 +32,6 @@ const App = () => {
       </div>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
