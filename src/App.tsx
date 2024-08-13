0980a610a45643435bfefa1de55f23f23f73ab1e
Leandro <lecamilotti@gmail.com>
2024-08-13 20:50:01 +0200

    add footer

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,6 @@ import {
 
 const App: React.FC = () => {
   const [isMobile, setIsMobile] = useState(false);
-  console.log(isMobile);
 
   useEffect(() => {
     const handleResize = () => {
@@ -32,21 +31,30 @@ const App: React.FC = () => {
 
   return (
     <BrowserRouter>
-      <div className="bg-red-500">
-        <div className="relative z-0  mb-0  bg-gradient-to-r from-blue-900 to-black pb-0">
-          <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-            <Navbar />
-            <Hero />
+      {isMobile && (
+        <div className="bg-gradient-to-r from-blue-900 to-black text-white p-4">
+          <div className="flex justify-between items-center bg-red-500 p-5 mt-20 mb-0">
+            <p className="text-center text-white ">
+              Due to the extensive use of 3D objects, this website is not
+              optimized for mobile devices. I strongly recommend using a desktop
+              or laptop for the best experience.
+            </p>
           </div>
-          <About />
-          <Experience />
-          <Tech />
-          <Works />
-          <div
-            className={`w-full mt-50 pb-0 z-10 ${styles.padding} min-h-[300px]`}
-          >
-            <Contact />
-          </div>
+        </div>
+      )}
+      <div className="relative z-0  mb-0  bg-gradient-to-r from-blue-900 to-black pb-0">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <div
+          className={`w-full mt-50 pb-0 z-10 ${styles.padding} min-h-[300px]`}
+        >
+          <Contact />
         </div>
       </div>
     </BrowserRouter>
--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,13 +1,14 @@
-import { useEffect, useRef, useState } from 'react';
-import emailjs from '@emailjs/browser';
-import { motion } from 'framer-motion';
-import { styles } from '../styles';
-import { SectionWrapper } from '../hoc';
-import { ISSCanvas } from './canvas';
+import { useEffect, useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { SectionWrapper } from "../hoc";
+import { ISSCanvas } from "./canvas";
+import { FaEnvelope, FaLinkedinIn } from "react-icons/fa";
 
 const Contact: React.FC = () => {
   const formRef = useRef<HTMLFormElement | null>(null);
-  const [form, setForm] = useState({ name: '', email: '', message: '' });
+  const [form, setForm] = useState({ name: "", email: "", message: "" });
   const [loading, setLoading] = useState<boolean>(false);
   const [showModal, setShowModal] = useState<boolean>(false);
   const [hovered, setHovered] = useState<boolean>(false);
@@ -29,9 +30,9 @@ const Contact: React.FC = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: 'Leandro Camilotti',
+          to_name: "Leandro Camilotti",
           from_email: form.email,
-          to_email: 'lecamilotti@gmail.com',
+          to_email: "lecamilotti@gmail.com",
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
@@ -39,14 +40,14 @@ const Contact: React.FC = () => {
       .then(
         () => {
           setLoading(false);
-          alert('Thank you. I will get back to you as soon as possible.');
-          setForm({ name: '', email: '', message: '' });
+          alert("Thank you. I will get back to you as soon as possible.");
+          setForm({ name: "", email: "", message: "" });
           setShowModal(false);
         },
         (error) => {
           setLoading(false);
           console.error(error);
-          alert('Ahh, something went wrong. Please try again.');
+          alert("Ahh, something went wrong. Please try again.");
         }
       );
   };
@@ -59,107 +60,123 @@ const Contact: React.FC = () => {
   }, [showModal]);
 
   return (
-    <div className='relative h-[60vh] pb-0 mb-0 '>
-      <motion.div
-        className='absolute inset-0 h-full z-0'
-        onPointerOver={() => {
-          if (!showModal) {
-            setHovered(true);
-          }
-        }}
-        onPointerOut={() => {
-          let timeout: ReturnType<typeof setTimeout>;
-          if (hovered) {
-            timeout = setTimeout(() => {
-              setHovered(false);
-            }, 2000);
-          }
-          return () => clearTimeout(timeout);
-        }}
-      >
-        <ISSCanvas />
-      </motion.div>
-      {hovered && (
-        <div className='absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-6 bg-black-100 bg-opacity-90 rounded-lg shadow-2xl z-10 min-w-80 text-center'>
-          <p className='text-center text-lg'>
-            Would you like to contact me or work with me?
-          </p>
-          <button
-            onClick={() => setShowModal(true)}
-            className='mt-2 px-4 py-2 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600 transition duration-300 text-center'
-          >
-            Click here
-          </button>
-        </div>
-      )}
-
-      {showModal && (
-        <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-90 z-10 '>
-          <div className='bg-black-100 p-8 rounded-2xl shadow-lg relative w-[600px]'>
+    <>
+      <div className="relative h-[60vh] pb-0 mb-0 ">
+        <motion.div
+          className="absolute inset-0 h-full z-0"
+          onPointerOver={() => {
+            if (!showModal) {
+              setHovered(true);
+            }
+          }}
+          onPointerOut={() => {
+            let timeout: ReturnType<typeof setTimeout>;
+            if (hovered) {
+              timeout = setTimeout(() => {
+                setHovered(false);
+              }, 2000);
+            }
+            return () => clearTimeout(timeout);
+          }}
+        >
+          <ISSCanvas />
+        </motion.div>
+        {hovered && (
+          <div className="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-6 bg-black-100 bg-opacity-90 rounded-lg shadow-2xl z-10 min-w-80 text-center">
+            <p className="text-center text-lg">
+              Would you like to contact me or work with me?
+            </p>
             <button
-              onClick={() => setShowModal(false)}
-              className='absolute top-4 right-4 text-gray-100 hover:text-gray-900 transition'
+              onClick={() => setShowModal(true)}
+              className="mt-2 px-4 py-2 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600 transition duration-300 text-center"
             >
-              &times;
+              Click here
             </button>
-            <p className={styles.sectionSubText}>Get in touch</p>
-            <form
-              ref={formRef}
-              onSubmit={handleSubmit}
-              className='mt-12 flex flex-col gap-8 '
-            >
-              <label className='flex flex-col'>
-                <span className='text-gray-700 font-medium mb-4'>
-                  Your Name
-                </span>
-                <input
-                  type='text'
-                  name='name'
-                  value={form.name}
-                  onChange={handleChange}
-                  placeholder="What's your good name?"
-                  className='bg-gray-200 py-4 px-6 placeholder-gray-500 text-gray-800 rounded-lg outline-none border-none font-medium'
-                />
-              </label>
-              <label className='flex flex-col'>
-                <span className='text-gray-700 font-medium mb-4'>
-                  Your Email
-                </span>
-                <input
-                  type='email'
-                  name='email'
-                  value={form.email}
-                  onChange={handleChange}
-                  placeholder="What's your email address?"
-                  className='bg-gray-200 py-4 px-6 placeholder-gray-500 text-gray-800 rounded-lg outline-none border-none font-medium'
-                />
-              </label>
-              <label className='flex flex-col'>
-                <span className='text-gray-700 font-medium mb-4'>
-                  Your Message
-                </span>
-                <textarea
-                  rows={7}
-                  name='message'
-                  value={form.message}
-                  onChange={handleChange}
-                  placeholder='What do you want to say?'
-                  className='bg-gray-200 py-4 px-6 placeholder-gray-500 text-gray-800 rounded-lg outline-none border-none font-medium'
-                />
-              </label>
-
-              <button
-                type='submit'
-                className='bg-blue-500 py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md hover:bg-blue-600 transition duration-300 '
-              >
-                {loading ? 'Sending...' : 'Send'}
-              </button>
-            </form>
           </div>
+        )}
+
+        {showModal && (
+          <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-90 z-10 ">
+            <div className="bg-black-100 p-8 rounded-2xl shadow-lg relative w-[600px]">
+              <button
+                onClick={() => setShowModal(false)}
+                className="absolute top-4 right-4 text-gray-100 hover:text-gray-900 transition"
+              >
+                &times;
+              </button>
+              <p className={styles.sectionSubText}>Get in touch</p>
+              <form
+                ref={formRef}
+                onSubmit={handleSubmit}
+                className="mt-12 flex flex-col gap-8 "
+              >
+                <label className="flex flex-col">
+                  <span className="text-gray-700 font-medium mb-4">
+                    Your Name
+                  </span>
+                  <input
+                    type="text"
+                    name="name"
+                    value={form.name}
+                    onChange={handleChange}
+                    placeholder="What's your good name?"
+                    className="bg-gray-200 py-4 px-6 placeholder-gray-500 text-gray-800 rounded-lg outline-none border-none font-medium"
+                  />
+                </label>
+                <label className="flex flex-col">
+                  <span className="text-gray-700 font-medium mb-4">
+                    Your Email
+                  </span>
+                  <input
+                    type="email"
+                    name="email"
+                    value={form.email}
+                    onChange={handleChange}
+                    placeholder="What's your email address?"
+                    className="bg-gray-200 py-4 px-6 placeholder-gray-500 text-gray-800 rounded-lg outline-none border-none font-medium"
+                  />
+                </label>
+                <label className="flex flex-col">
+                  <span className="text-gray-700 font-medium mb-4">
+                    Your Message
+                  </span>
+                  <textarea
+                    rows={7}
+                    name="message"
+                    value={form.message}
+                    onChange={handleChange}
+                    placeholder="What do you want to say?"
+                    className="bg-gray-200 py-4 px-6 placeholder-gray-500 text-gray-800 rounded-lg outline-none border-none font-medium"
+                  />
+                </label>
+
+                <button
+                  type="submit"
+                  className="bg-blue-500 py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md hover:bg-blue-600 transition duration-300 "
+                >
+                  {loading ? "Sending..." : "Send"}
+                </button>
+              </form>
+            </div>
+          </div>
+        )}
+      </div>
+      <footer className="flex items-center justify-center h-16 mt-5 ">
+        <div className="flex items-center space-x-3">
+          <a href="mailto:lecamilotti@gmail.com" target="_blank">
+            <FaEnvelope className="text-white-600 hover:text-gray-400 transition" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/leandrocamilotti/"
+            target="_blank"
+          >
+            <FaLinkedinIn className="text-white-600 hover:text-gray-400 transition mr-2" />
+          </a>
         </div>
-      )}
-    </div>
+        <p className="text-white">Developed by Leandro Camilotti 2024</p>
+      </footer>
+    </>
   );
 };
 
-export default SectionWrapper({ idName: 'contact', Component: Contact });
+export default SectionWrapper({ idName: "contact", Component: Contact });
